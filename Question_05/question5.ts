e089c0f4a21997aafc67b26ed0c1b5a4ca92719d
malickyasin <160276204+malickyasin@users.noreply.github.com>
2024-03-07 18:16:23 +0500

    Add files via upload

---
+++ Question_05/question5.ts
@@ -0,0 +1,9 @@
+
+let famouspersons: string = "Albert Einstein";
+let famousquote: string = '"A person who never made a mistake never tried anything new."';
+
+// compose message
+
+let message: string = `${famouspersons} onec said, ${famousquote}`;
+
+console.log(message);
